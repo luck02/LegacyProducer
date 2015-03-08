07b8d5fc7b52a070ea506c1b2254102ea86ea447
Gary Lucas <glucas@paybyphone.com>
2015-03-07 23:03:27 -0800

    wke-123 can push events

--- LegacyProducer/LegacyProducer.go
+++ LegacyProducer/LegacyProducer.go
@@ -4,7 +4,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"os"
+
+	"github.com/Shopify/sarama"
 )
 
 type LegacyEvents struct {
@@ -17,7 +20,19 @@ type LegacyEvent struct {
 	KeyValue   int
 }
 
+func InitLogger() {
+	logFile, err := os.OpenFile("sarama.debug.log", os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		log.Fatalln("Failed to open log file:", err)
+	}
+
+	sarama.Logger = log.New(logFile,
+		"PREFIX: ",
+		log.Ldate|log.Ltime|log.Lshortfile)
+}
+
 func main() {
+	InitLogger()
 	file, e := ioutil.ReadFile("./TestInput.json")
 	if e != nil {
 		fmt.Printf("FIle error: %v\n", e)
@@ -38,9 +53,35 @@ func main() {
 }
 
 func PublishEvents(legacyEvents LegacyEvents) {
+	producer, err := sarama.NewProducer([]string{"localhost:9092"}, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		if err := producer.Close(); err != nil {
+			panic(err)
+		}
+	}()
+
 	for index, element := range legacyEvents.LegacyEvents {
 		fmt.Print(index)
 		fmt.Print(element)
 		fmt.Println()
+
+		elementBytes, err := json.Marshal(element)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		elementString := elementBytes[:]
+		select {
+		case producer.Input() <- &sarama.ProducerMessage{Topic: "LegacyEvents", Key: nil, Value: sarama.StringEncoder(elementString)}:
+			fmt.Println(">Legacy Events QUeued")
+		case err := <-producer.Errors():
+			panic(err.Err)
+
+		}
+
 	}
 }
